98e1c478c7ff997446835d81e091a13fa5f5b219
resq-u <jakub12lepianka@wp.pl>
2025-12-19 16:29:30 +0100

    Add provisional Homepage component with styling for mobile

---
+++ src/components/paths/HomePage/Home.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router";
+import styles from "./Home.module.css";
+import bgSmall from "../../../assets/background/bg-small.jpeg";
+import bgFull from "../../../assets/background/bg-full.jpeg";
+
+
+export const Home = () => {
+  return (
+    <>
+      <div>
+        <img 
+          src={bgSmall}
+          srcSet={`${bgSmall} 1024w, ${bgFull} 1408w`}
+          sizes="(max-width: 599px) 100vw, 1408px"
+          className={styles.bgImg} 
+          alt="smartphone and accessories on tealish background"
+        />
+      </div>
+      <div className={styles.homeMainOne}>
+        <div className={styles.headingContainer}>
+          <h1 className={styles.mainHeading}>Accessories that keep up with your phone</h1>
+          <Link to="/shop" className={styles.shopLink}>
+            Shop Now !
+          </Link>
+        </div>
+      </div>
+      <div className={styles.homeMainTwo}>
+        <h1>This is a second part</h1>
+      </div>
+    </>
+  );
+};
