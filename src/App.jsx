4ab154e2c81b10a2d88bef5a4ade97bb5a62474c
resq-u <jakub12lepianka@wp.pl>
2025-12-20 13:05:42 +0100

    Add Background, Header and Outlet components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { Outlet } from "react-router";
+import { Header } from "./components/Header/Header.jsx";
+import { Background } from "./components/Background/Background.jsx";
 
 function App() {
 
   return (
     <>
+      <Background />
+      <Header></Header>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
