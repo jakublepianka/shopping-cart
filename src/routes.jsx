eebc5232aca0d93b07fb65639c43f294a441841e
resq-u <jakub12lepianka@wp.pl>
2025-12-20 13:04:27 +0100

    Add routes for react-router

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,18 @@
 import App from "./App";
+import { Home } from "./components/paths/HomePage/Home";
+import { Shop } from "./components/paths/ShopPage/Shop";
+import { Cart } from "./components/paths/CartPage/Cart";
 
 const routes = [
   {
     path: "/",
     element: <App />,
-  }
-]
+    children: [
+      { index: true, element: <Home /> },
+      { path: "shop", element: <Shop /> },
+      { path: "cart", element: <Cart /> },
+    ],
+  },
+];
 
 export default routes;
