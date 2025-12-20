adc0f4ce23a6dce30f1574482b2901a608607c72
resq-u <jakub12lepianka@wp.pl>
2025-12-20 13:03:03 +0100

    Add scaffold path components for Cart and Shop pages

---
+++ src/components/paths/CartPage/Cart.jsx
@@ -0,0 +1,9 @@
+import styles from "./Cart.module.css";
+
+export const Cart = () => {
+  return (
+    <div className={styles.cartContainer}>
+      Cart page
+    </div>
+  );
+}
---
+++ src/components/paths/ShopPage/Shop.jsx
@@ -0,0 +1,9 @@
+import styles from "./Shop.module.css"
+
+export const Shop = () => {
+  return (
+    <div className={styles.shopMain}>
+      Shop page
+    </div>
+  );
+}
