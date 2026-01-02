0c3d5220f276ac4c3ebd1981f91346ffe9b34b07
resq-u <jakub12lepianka@wp.pl>
2026-01-02 16:57:16 +0100

    Add products context, filtered images array & Carousel component

--- src/components/paths/HomePage/Home.jsx
+++ src/components/paths/HomePage/Home.jsx
@@ -1,15 +1,27 @@
 import { Link } from "react-router";
+import { Carousel } from "../../Carousel/Carousel";
+import { useProducts } from "../../../context/useProducts";
 import styles from "./Home.module.css";
-import bgSmall from "../../../assets/background/bg-small.jpeg";
-import bgFull from "../../../assets/background/bg-full.jpeg";
-
 
 export const Home = () => {
+  const { products } = useProducts();
+  const images = (products || [])
+    .filter((product) => product.rating > 4.4)
+    .map((product) => product.thumbnail);
+
+  const carouselOptions = {
+    isAuto: true,
+    transitionDuration: 1000,
+    transitionDelay: 6000,
+  };
+
   return (
     <>
       <div className={styles.homeMainOne}>
         <div className={styles.headingContainer}>
-          <h1 className={styles.mainHeading}>Accessories that keep up with your phone</h1>
+          <h1 className={styles.mainHeading}>
+            Accessories that keep up with your phone
+          </h1>
           <Link to="/shop" className={styles.shopLink}>
             Shop Now !
           </Link>
@@ -17,6 +29,7 @@ export const Home = () => {
       </div>
       <div className={styles.homeMainTwo}>
         <h1>This is a second part</h1>
+        <Carousel images={images} carouselOptions={carouselOptions}></Carousel>
       </div>
     </>
   );
