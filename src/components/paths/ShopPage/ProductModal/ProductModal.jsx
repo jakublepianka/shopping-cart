3f79c9b1b8c764e448bc72964235df5e96423e41
resq-u <jakub12lepianka@wp.pl>
2026-01-08 11:28:04 +0100

    Add initial ProductModal structure and styling

--- src/components/paths/ShopPage/ProductModal/ProductModal.jsx
+++ src/components/paths/ShopPage/ProductModal/ProductModal.jsx
@@ -1,12 +1,50 @@
 import styles from "./ProductModal.module.css";
+import carouselStyles from "./Carousel.module.css";
+import plusIcon from "../../../../assets/icons/plus.png";
+import { Carousel } from "../../../Carousel/Carousel";
+import { AddToCartControls } from "./AddToCartControls/AddToCartControls";
 
-export const ProductModal = ({product}) => {
+export const ProductModal = ({ product, onClose }) => {
+  const images = product.images || [];
+  const carouselOptions = {
+    isAuto: false,
+    transitionDuration: 250,
+  };
+
   return (
     <>
       <div className={styles.modalBackdrop}></div>
       <div className={styles.modalCard}>
-        <h1>{product.title}</h1>
-        
+        <h1 className={styles.productName}>{product.title}</h1>
+        <button
+          onClick={onClose}
+          className={styles.closeButton}
+          aria-label="Close"
+        >
+          <img src={plusIcon} className={styles.closeIcon} alt="" />
+        </button>
+        <Carousel
+          images={images}
+          carouselOptions={carouselOptions}
+          showSideNav={false}
+          carouselClassNames={{ carousel: carouselStyles.carousel, carouselSection: carouselStyles.carouselSection }}
+          overlayClassNames={
+            {
+              goToButtonsContainer: carouselStyles.goToButtonsContainer,
+              goToButtonWrapper: carouselStyles.goToButtonWrapper,
+              goToButton: carouselStyles.goToButton,
+              goToButtonCurrent: carouselStyles.goToButtonCurrent
+            }
+          }
+        ></Carousel>
+        <div className={styles.body}>
+          <AddToCartControls availableQuantity={product.stock}/>
+
+            <section className={styles.section}>
+              <h2 className={styles.sectionHeading}>Description</h2>
+              <p className={styles.bodyText}>{product.description}</p>
+            </section>
+        </div>
       </div>
     </>
   );
