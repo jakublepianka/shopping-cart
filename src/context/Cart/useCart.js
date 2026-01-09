33c94f0caf67d863e296c0a274d9e8575856177e
resq-u <jakub12lepianka@wp.pl>
2026-01-09 17:53:15 +0100

    Add sumQuantity & sumPrice functions

--- src/context/Cart/useCart.js
+++ src/context/Cart/useCart.js
@@ -4,40 +4,6 @@ import { CartContext } from "./CartContext";
 export const useCart = () => {
   const { cart, setCart } = useContext(CartContext);
 
-  // const addToCart = (product) => {
-  //   setCart((prev) => {
-  //     const found = prev.find((item) => item.id === product.id);
-  //     const old = prev.filter((item) => item.id !== product.id);
-  //     if (
-  //       found &&
-  //       found.quantity + product.quantity < product.availableQuantity
-  //     ) {
-  //       const newProduct = {
-  //         ...found,
-  //         quantity: found.quantity + product.quantity,
-  //       };
-  //       return [...old, newProduct];
-  //     } else if (prev.length > 0) return [...prev, { ...product }];
-  //     else return [{ ...product }];
-  //   });
-  // };
-
-  // const addToCart = (product) => {
-  //   setCart((prev) => {
-  //     const found = prev.find((item) => item.id === product.id);
-  //     if (!found) return [...prev, product];
-  //     if (found.quantity + product.quantity <= product.availableQuantity) {
-  //       const cleanedArr = prev.filter((item) => item.id !== found.id);
-  //       const newProduct = {
-  //         ...found,
-  //         quantity: found.quantity + product.quantity,
-  //       };
-  //       return [...cleanedArr, newProduct];
-  //     }
-  //     return prev;
-  //   });
-  // };
-
   const addToCart = (product) => {
     setCart((prev) => {
       const cartMap = new Map(prev.map((item) => [item.id, item]));
@@ -59,9 +25,14 @@ export const useCart = () => {
     });
   };
 
-  const isDuplicate = (product) => cart.some((item) => item.id === product.id);
+  const sumQuantity = () => cart.reduce((acc, curr) => acc + curr.quantity, 0);
+
+  const sumPrice = () => cart.reduce((acc, curr) => acc + curr.price, 0);
+
   return {
     cart,
     addToCart,
+    sumQuantity,
+    sumPrice,
   };
 };
