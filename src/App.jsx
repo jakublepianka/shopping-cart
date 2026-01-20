6c86d05d222fe4e2fb07d56acfaf2dc42f40d874
resq-u <jakub12lepianka@wp.pl>
2026-01-20 21:13:29 +0100

    Remove Background component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router";
 import { Header } from "./components/Header/Header.jsx";
-import { Background } from "./components/Background/Background.jsx";
 import { CartProvider } from "./context/Cart/CartProvider.jsx";
 
 function App() {
   return (
     <>
-      <Background />
       <CartProvider>
         <Header></Header>
         <main>
